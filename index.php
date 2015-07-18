bf6971e19938d5c090083b086d179520fa0100f1
Francisco Santos <franciscosantosinfo@gmail.com>
2015-07-18 16:36:18 -0300

    This is one simple system login in php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<title>Login</title>
+		<link type="text/css" rel="stylesheet" href="layout.css">
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<div id="form">
+			<form method="post" action="login.php">
+				<input type="text" name="user" class="user" placeholder="User">
+				<input type="pass" name="pass" class="pass" placeholder="Password">
+				<input type="submit" value="Login" class="login">
+			</form>			
+		</div>
+	</body>
+</html>
