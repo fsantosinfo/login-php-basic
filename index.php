9a4b05db8cecaedb81bf17bb26eb80cf526d6483
Francisco Santos <franciscosantosinfo@gmail.com>
2015-07-18 17:37:06 -0300

    New work

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 	<body>
 		<div id="form">
 			<form method="post" action="login.php">
+				<img src="img/login.gif" width="32"> <h1>Company Login</h1>
 				<input type="text" name="user" class="user" placeholder="User">
-				<input type="pass" name="pass" class="pass" placeholder="Password">
+				<input type="password" name="pass" class="pass" placeholder="Password">
 				<input type="submit" value="Login" class="login">
 			</form>			
 		</div>
